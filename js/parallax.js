ebe8641d483011ef9c15ac5601813b1d6cdc0365
Emiliano Frese <emilianomartindominguezfrese@gmail.com>
2021-08-28 18:35:37 -0300

    JJBLOMAS

---
+++ js/parallax.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        var windowWidth = $(window).width();
+
+        if (windowWidth > 800){
+            var scroll = $(window).scrollTop();
+
+            $('header .textos').css({
+                'transform': 'translate(0px,' + scroll / 3 + '%)'
+            });
+
+            $('.acerca-de article').css({
+                'transform': 'translate(0px, -' + scroll / 5 + '%)'
+            });
+        }
+    });
+
+    $(window).resize(function(){
+        var windowWidth = $(window).width();
+
+        if(windowWidth < 800) {
+
+            $('header .textos').css({
+                'transform': 'translate(0px, 0px)'
+            });
+
+            $('.acerca-de article').css({
+                'transform': 'translate(0px, 0px)'
+            });
+        }
+    });
+
+});
